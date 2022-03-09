a6f6f11a5bb0101e30b110d4e725baae3b5fa1ed
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-03-09 19:21:52 +0100

    implement router interface

--- src/Router.php
+++ src/Router.php
@@ -2,13 +2,14 @@
 
 namespace Bone\Router;
 
+use Bone\Http\RouterInterface;
 use League\Route\Route;
 use League\Route\Router as LeagueRouter;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class Router extends LeagueRouter implements RequestHandlerInterface
+class Router extends LeagueRouter implements RequestHandlerInterface, RouterInterface
 {
     /**
      * @return Route[]
